f4a522be98cbc76fb8bcd9f4732127fcffefc243
9Hope9 <BasicHope9@example.com>
2024-07-07 18:25:25 -0400

    add get and post method for schema

--- node.js
+++ node.js
@@ -59,6 +59,132 @@ const productSchema = new mongoose.Schema({
   const Order = mongoose.model('Order', orderSchema);
   
 
+// Product Routes
+app.post('/products', async (req, res) => {
+    try {
+      const product = new Product(req.body);
+      await product.save();
+      res.status(201).send(product);
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  });
+  
+  app.get('/products', async (req, res) => {
+    try {
+      const products = await Product.find();
+      res.status(200).send(products);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  });
+  
+  app.get('/products/:id', async (req, res) => {
+    try {
+      const product = await Product.findById(req.params.id);
+      if (!product) {
+        return res.status(404).send();
+      }
+      res.status(200).send(product);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  });
+  
+  // User Routes
+  app.post('/users', async (req, res) => {
+    try {
+      const user = new User(req.body);
+      await user.save();
+      res.status(201).send(user);
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  });
+  
+  app.get('/users', async (req, res) => {
+    try {
+      const users = await User.find();
+      res.status(200).send(users);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  });
+  
+  app.get('/users/:id', async (req, res) => {
+    try {
+      const user = await User.findById(req.params.id);
+      if (!user) {
+        return res.status(404).send();
+      }
+      res.status(200).send(user);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  });
+  
+  // Comment Routes
+  app.post('/comments', async (req, res) => {
+    try {
+      const comment = new Comment(req.body);
+      await comment.save();
+      res.status(201).send(comment);
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  });
+  
+  app.get('/comments', async (req, res) => {
+    try {
+      const comments = await Comment.find().populate('product').populate('user');
+      res.status(200).send(comments);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  });
+  
+  // Cart Routes
+  app.post('/carts', async (req, res) => {
+    try {
+      const cart = new Cart(req.body);
+      await cart.save();
+      res.status(201).send(cart);
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  });
+  
+  app.get('/carts/user/:userId', async (req, res) => {
+    try {
+      const cart = await Cart.findOne({ user: req.params.userId }).populate('products.product');
+      if (!cart) {
+        return res.status(404).send();
+      }
+      res.status(200).send(cart);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  });
+  
+  // Order Routes
+  app.post('/orders', async (req, res) => {
+    try {
+      const order = new Order(req.body);
+      await order.save();
+      res.status(201).send(order);
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  });
+  
+  app.get('/orders', async (req, res) => {
+    try {
+      const orders = await Order.find().populate('user').populate('products.product');
+      res.status(200).send(orders);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  });
 
 // Start server
 app.listen(PORT, () => {
