041b99a6da51f1e3b487ec405657aaff6f8c2c34
9Hope9 <BasicHope9@example.com>
2024-07-07 21:48:01 -0400

    End Of The Project

--- node.js
+++ node.js
@@ -7,7 +7,8 @@ const PORT = 5000;
 app.use(bodyParser.json());
 
 // MongoDB setup
-const mongoURI = 'mongodb+srv://ajadav1971:d82iTE5woXTm62NZ@cluster0.o5vlzvn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'; 
+
+const mongoURI = 'mongodb+srv://ajadav1971:d82iTE5woXTm62NZ@cluster0.o5vlzvn.mongodb.net/assignment2?retryWrites=true&w=majority&appName=Cluster0'; 
 mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
